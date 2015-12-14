c20561bbf9cf6ccd9fb6e5698f0176616bbfcc82
Johan Anderstedt <jumpi_inn@hotmail.com>
2015-12-14 11:58:15 +0100

    Ny kod
    Kod till tunna, kanon och spikarna. Ändringen i Collision scriptet gör
    att spikarna kan skada Pac-Man om de har tagen "Trap", har en kolider
    och kolidern är "Is Trigger".

---
+++ Barrel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+	public GameObject plank;
+	public float speed = 2.0f;
+	void Update()
+	{
+		transform.Translate (transform.forward * speed * Time.deltaTime, Space.World);	
+	}
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.tag == "Wall")
+		{
+			Instantiate (plank, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+		else if (other.gameObject.tag == "Player")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
